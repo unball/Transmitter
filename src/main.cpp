2565690c3e8654f42d2a21a92849e15e119bc3f4
Luiz <labm1997@gmail.com>
2019-12-21 15:02:52 -0300

    Implementado primeira versão de protocolo para o rádio que não utiliza o ROS

--- src/main.cpp
+++ src/main.cpp
@@ -1,95 +1,136 @@
-#include <SPI.h> //for radio
-#include "RF24.h" // for radio
-#include <ros.h>
+#include <SPI.h>
+#include "RF24.h"
 #include <ArduinoHardware.h>
-#include <ros_lib/ros_lib/main_system/robot_msg.h>
 
-void radioSetup();
-void repeteVelocidade();
-
+/* Definições */
 #define NUMBER_OF_ROBOTS 5
-#define NANO
-#ifndef NANO
-#define PRO_MICRO
-#endif
-/*************  USER Configuration *****************************/
-/***********radio***********/
-/*Radio pins*/
-#ifdef PRO_MICRO
-int CE = 10;
-int CS = A0;
-#else //nano
+
+/* Declaração das funções */
+void radioSetup();
+void enviaMensagem();
+void atualizaVelocidades();
+
+/* Pinos para o rádio */
 int CE = 3;
 int CS = 2;
-#endif
-                           
+
+/* Objeto que gerencia o rádio */
 RF24 radio(CE,CS);
 
-/*const uint8_t addresses [6][5] = {{0xAB, 0xCD, 0xAB, 0xCD, 0x71},
-                                                  {0x54, 0x4D, 0x52, 0x68, 0x72},
-                                                  {0x54, 0x4D, 0x52, 0x68, 0x73},
-                                                  {0x54, 0x4D, 0x52, 0x68, 0x74},
-                                                  {0x54, 0x4D, 0x52, 0x68, 0x75},
-                                                  {0x54, 0x4D, 0x52, 0x68, 0x76}};*/
-const uint64_t addresses[4] = { 0xABCDABCD71L, 0x544d52687CL, 0x644d52687CL, 0x744d52687CL };
-uint64_t pipeRecebe = addresses[3];
+/* Endereços */
+uint64_t txPipeAddress = 0xABCDABCD71L;
+uint64_t rxPipeAddress = 0x744d52687CL;
 
-ros::NodeHandle nh;
-
+/* Estrutura para a mensagem a ser transmitida */
 struct Velocidade{
   int16_t motorA[5];
   int16_t motorB[5];
 };
 
+/* Estrutura para a mensagem a ser recebida do USB */
+struct VelocidadeSerial {
+  Velocidade data;
+  uint16_t checksum;
+};
+
+/* Mensagem a ser transmitida */
 Velocidade velocidades;
 
-int menu;
-void motorVel( const main_system::robot_msg& velocidadesdata){
-   menu = velocidadesdata.menu;
-   for (int i=0; i<NUMBER_OF_ROBOTS; i++) {
-    velocidades.motorA[i] = velocidadesdata.MotorA[i];
-    velocidades.motorB[i] = velocidadesdata.MotorB[i];
-   }
-}
+/* Contagem de erros de transmissão via USB detectados */
+uint32_t erros = 0;
 
-ros::Subscriber<main_system::robot_msg> sub("radio_topic", &motorVel);
-
-String inString = ""; 
-/***************************************************************/
-
+/* Loop de setup */
 void setup(void) {
   Serial.begin(115200);
   radioSetup();
-  nh.getHardware()->setBaud(115200);
-  nh.initNode();
-  nh.subscribe(sub);
 }
 
-
+/* Loop que é executado continuamente */
 void loop(){
-  nh.spinOnce();
-  repeteVelocidade();
+  atualizaVelocidades();
+  enviaMensagem();
 }
 
-void repeteVelocidade(){
+/* Envia a mensagem pelo rádio */
+void enviaMensagem(){
+  // Garante que o rádio não está escutando
   radio.stopListening();
+
+  // Permite chamadas de write sem ACK
   radio.enableDynamicAck();
-  radio.openWritingPipe(addresses[0]);
+
+  // Abre um pipe para escrita
+  radio.openWritingPipe(txPipeAddress);
+
+  // Envia a mensagem
   radio.write(&velocidades,sizeof(velocidades), 1);
-  //velocidades[i].motorA = 0;
-  //velocidades[i].motorB = 0;
 }
 
+/* Configura o rádio */
 void radioSetup(){
-  radio.begin();                           // inicializa radio
-  radio.setChannel(108);                   //muda para um canal de frequencia diferente de 2.4Ghz
-  radio.setPALevel(RF24_PA_MAX);           //usa potencia maxima
-  radio.setDataRate(RF24_2MBPS);           //usa velocidade maxima
+  // inicializa radio
+  radio.begin();
 
-  radio.openReadingPipe(1,pipeRecebe);      //escuta pelo pipe1
+  // muda para um canal de frequencia diferente de 2.4Ghz
+  radio.setChannel(108);
 
-  radio.enableDynamicPayloads();           //ativa payloads dinamicos(pacote tamamhos diferentes do padrao)
-  radio.setPayloadSize(sizeof(velocidades));   //ajusta os o tamanho dos pacotes ao tamanho da mensagem
+  // usa potencia maxima
+  radio.setPALevel(RF24_PA_MAX);
+
+  // usa velocidade maxima
+  radio.setDataRate(RF24_2MBPS);
+
+  // escuta pelo pipe1
+  radio.openReadingPipe(1,rxPipeAddress);
+
+  // ativa payloads dinamicos(pacote tamamhos diferentes do padrao)
+  radio.enableDynamicPayloads();
+
+  // ajusta o tamanho dos pacotes ao tamanho da mensagem
+  radio.setPayloadSize(sizeof(velocidades));
   
-  radio.startListening();                 // Start listening
+  // Start listening
+  radio.startListening();
+}
+
+/* Lê do serial novas velocidades */
+void atualizaVelocidades(){
+  int initCounter = 0;
+  while(Serial.available()){
+    /* Lê um caracter da mensagem */
+    char character = Serial.read();
+
+    /* Incrementa o contador se for 'B' */
+    if(character == 'B') initCounter++;
+
+    /* Se os três primeiros caracteres são 'B' então de fato é o início da mensagem */
+    if(initCounter >= 3){
+      VelocidadeSerial receber;
+      
+      /* Lê a mensagem até o caracter de terminação e a decodifica */
+      Serial.readBytes((char*)(&receber), (size_t)sizeof(VelocidadeSerial));
+
+      /* Faz o checksum */
+      uint16_t checksum = 0;
+      for(int i=0 ; i<5 ; i++){
+        checksum += receber.data.motorA[i] + receber.data.motorB[i];
+      }
+
+      /* Verifica o checksum */
+      if(checksum == receber.checksum){
+        /* Copia para o buffer global de velocidades */
+        velocidades = receber.data;
+
+        /* Reporta que deu certo */
+        Serial.println("OK");
+      }
+      else {
+        /* Devolve o checksum calculado se deu errado */
+        Serial.println(checksum);
+      }
+
+      /* Zera o contador */
+      initCounter = 0;
+    }
+  }
 }
