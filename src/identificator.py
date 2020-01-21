5d15db3d05bc7b141319fed34647cd2bae188809
Luiz <labm1997@gmail.com>
2020-01-21 14:05:31 -0300

    Identificador refatorado

--- src/identificator.py
+++ src/identificator.py
@@ -1,19 +1,12 @@
-import serial
-import signal
+from serialReader import SerialReader
+from scipy.optimize import curve_fit
 
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.optimize import curve_fit
 
-ser = serial.Serial("/dev/ttyUSB0", 115200)
+BUFFER_SIZE = 1000
 
-data = np.array([[0,0,0,0,0]])
-mainData = np.array([[0,0,0,0,0]])
-points = 998
-poitstoremove = 50
-first = True
-stop = False
-
+# Função que otimiza os parâmetros para uma resposta de primeira ordem
 def find_params(xdata, ydata, A):
     def response(x, k, tau):
         return A*k*(1-np.exp(-x/tau))                                       
@@ -21,72 +14,67 @@ def find_params(xdata, ydata, A):
     popt, pcov = curve_fit(response, xdata, ydata)
     return popt, response(xdata, *popt)
 
-def write():
-    global stop
-    np.savetxt("output", data, delimiter=',')
-    stop = True
-
-while not stop:
-    
-    line = ser.readline().decode()
-    splitted = line.replace('\n','').split(" ")
-
-    try:
-        if first:
-            print("Recebendo...")
-            first = False
-        values = [float(x) for x in splitted]
-        data = np.concatenate((data,[values]))
-    except:
-        pass
-
-    if data[:,0].size % points == 0:
-        write()
-        data = data[1:]
-        data[:,0] -= data[:,0][0]
-        data[:,0] /= 1000000.0
-
-        # Motor A
+# Estima os parâmetros da planta com base nos dados em data, onde data[:,0] é tempo, data[:,1] é velocidade e data[:,2] é encoder
+def parameterEstimator(data):
         
-        # Gráfico inteiro
-        plt.subplot(2,2,1)
-        plt.plot(data[:,0], data[:,1], color='k')
-        plt.plot(data[:,0], data[:,3], color='r')
+    # Gráfico inteiro
+    plt.subplot(2,2,1)
+    plt.plot(data[:,0], data[:,1], color='k')
+    plt.plot(data[:,0], data[:,2], color='r')
 
-        # Parte positiva
-        final = np.argmin(data[:,1] > 0)
-        datap = data[:final]
+    # Parte positiva
+    final = np.argmin(data[:,1] > 0)
+    datap = data[:final]
 
-        plt.subplot(2,2,2)
-        plt.plot(datap[:,0], datap[:,1], color='k')
-        plt.plot(datap[:,0], datap[:,3], color='r')
+    plt.subplot(2,2,2)
+    plt.plot(datap[:,0], datap[:,1], color='k')
+    plt.plot(datap[:,0], datap[:,2], color='r')
 
-        # Identifica
-        pA, rA = find_params(datap[:,0], datap[:,3], datap[:,1].max())
-        plt.plot(datap[:,0], rA, color='b')
-        print(pA)
-
-        # Motor B
-        
-        # Gráfico inteiro
-        plt.subplot(2,2,3)
-        plt.plot(data[:,0], data[:,2], color='k')
-        plt.plot(data[:,0], data[:,4], color='r')
-
-        # Parte positiva
-        final = np.argmin(data[:,1] > 0)
-        datap = data[:final]
-
-        plt.subplot(2,2,4)
-        plt.plot(datap[:,0], datap[:,2], color='k')
-        plt.plot(datap[:,0], datap[:,4], color='r')
-
-        # Identifica
-        pB, rB = find_params(datap[:,0], datap[:,4], datap[:,2].max())
-        plt.plot(datap[:,0], rB, color='b')
-        print(pB)
-
-        plt.show()
-
-        break
+    # Identifica
+    p, r = find_params(datap[:,0], datap[:,2], datap[:,1].max())
+    plt.plot(datap[:,0], r, color='b')
     
+    return p
+
+def main():
+    # Recebe do rádio
+    serial = SerialReader("/dev/ttyUSB0", 115200)
+
+    # Recebe do direto do teensy via USB
+    #serial = SerialReader("/dev/ttyACM0", 9600)
+
+    # Dados a serem recebidos
+    data = []
+
+    # Recebe um buffer de dados
+    while data.size < BUFFER_SIZE-2:
+        values = serial.read()
+
+        # Concatena ao conjunto de dados lidos
+        if values is not None:
+            data.append(values)
+
+    # Converte para numpy
+    data = np.array(data)
+
+    # Salva os dados em um arquivo
+    np.savetxt("deadzonedata.dat", data, delimiter=',')
+
+    # Corrige os dados para segundos
+    data[:,0] -= data[:,0][0]
+    data[:,0] /= 1000000.0
+
+    # Estima a planta do motor A
+    p = parameterEstimator(np.array([data[:,0], data[:,1], data[:,3]]).T)
+    print("Motor A:")
+    print(p)
+
+    # Estima a planta do motor B
+    p = parameterEstimator(np.array([data[:,0], data[:,2], data[:,4]]).T)
+    print("Motor B:")
+    print(p)
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
