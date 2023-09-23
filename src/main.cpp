b4ca053668819f96ada9c06fad8aba6844f0141d
André <andredornelas23@gmail.com>
2023-09-23 19:08:10 -0300

    muda channel e power

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,19 @@
 
 /* Definições */
 #define NUMBER_OF_ROBOTS 3
+#define MAX_POWER 10.5  //TODO: Testar valores
+#define WIFI_CHANNEL 12  //TODO: Testar valores
 
 //Endereço de broadcast,FF pois envia para todos
 uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
-/* Estrutura para a mensagem a ser transmitida para o robô via rádio */
+/* Estrutura para a mensagem a ser transmitida para o robô via wi-fi */
 struct Velocidade{
   int16_t vl[3];
   int16_t vr[3];
 };
 
-struct VelocidadeRadio{
+struct MensagemWifi{
   int16_t id;
   int16_t vl;
   int16_t vr;
@@ -80,18 +82,18 @@ void loop(){
 
 /* Envia a mensagem pelo rádio */
 void sendWifi(){
-  
+
   // Envia a mensagem
   result = true;
 
   for(uint8_t i=0 ; i<NUMBER_OF_ROBOTS ; i++){
-    VelocidadeRadio vel = {.id = i, .vl = velocidades.vl[i], .vr = velocidades.vr[i]};
+    MensagemWifi vel = {.id = i, .vl = velocidades.vl[i], .vr = velocidades.vr[i]};
     
     //Envia a mensagem usando o ESP-NOW
-    esp_now_send(broadcastAddress, (uint8_t *) &vel, sizeof(VelocidadeRadio));
+    esp_now_send(broadcastAddress, (uint8_t *) &vel, sizeof(MensagemWifi));
     delay(3);
-
   }
+  
   if(result){
     lastOK = millis();
   }
@@ -101,6 +103,7 @@ void sendWifi(){
 void wifiSetup(){
   //Coloca o dispositivo no modo Wi-Fi Station
   WiFi.mode(WIFI_STA);
+  WiFi.setOutputPower(MAX_POWER);
 
   //Inicializa o ESP-NOW
   if (esp_now_init() != 0) {
@@ -110,7 +113,7 @@ void wifiSetup(){
   esp_now_register_send_cb(OnDataSent);
 
   //Registra o destinatario da mensagem
-  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, WIFI_CHANNEL, NULL, 0);
 
 }
 
