9a024053f9c0666fb8c50202980b6338bbf73c10
André <andredornelas23@gmail.com>
2023-10-13 15:17:59 -0300

    remove diretiva de préprocessamento PID_TUNNER

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,6 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 
-#define PID_TUNNER false
-
 /* Definitions */
 #define MAX_POWER 10.5  //TODO: Test values
 #define WIFI_CHANNEL 12  //TODO: Test values
@@ -18,36 +16,12 @@ struct RobotMessage{
   int16_t w[3];
 };
 
-#if PID_TUNNER
-struct snd_message{
-  uint8_t id;
-  int16_t kp;
-  int16_t ki;
-  int16_t kd;
-};
-
-snd_message send_commands;
-
-struct rcv_message{
-  float value;
-};
-
-rcv_message rcv_commands;
-
-/* Struct for the message to be received from USB */
-struct SerialConstants {
-  snd_message data;
-  double checksum;
-};
-
-#else
 struct snd_message{
   uint8_t control;
   uint8_t id;
   int16_t v;
   int16_t w;
 };
-#endif
 
 /* Estrutura para a mensagem a ser recebida do USB */
 struct SerialMessage {
@@ -76,15 +50,6 @@ void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus)
   else
     result = false;
 }
-
-#if PID_TUNNER
-// Callback function, execute when message is sent via Wi-Fi
-void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len){
-  memcpy(&rcv_commands, incomingData, sizeof(rcv_commands));
-  Serial.println(rcv_commands.value);
-  delay(2);
-}
-#endif
 
 /* Loop de setup */
 void setup(void) {
@@ -116,101 +81,6 @@ void loop(){
 
 }
 
-#if PID_TUNNER
-/* Sends the message via Wi-Fi */
-void sendWifi(){
-
-  result = true;
-
-  for(uint8_t i=0 ; i<3 ; i++){
-    snd_message control_constants = {.id = i, .kp = 1.0, .ki = 1.0, .kd = 1.0};
-    
-    /* Sends the message using ESP-NOW */
-    esp_now_send(broadcastAddress, (uint8_t *) &control_constants, sizeof(snd_message));
-    delay(3);
-  }
-  
-  if(result){
-    lastOK = millis();
-  }
-}
-
-/* Setup the Wi-Fi  */
-void wifiSetup(){
-  /* Puts the device in Wi-Fi Station mode */
-  WiFi.mode(WIFI_STA);
-
-  WiFi.setOutputPower(MAX_POWER);
-
-  /* Initialize the ESP-NOW */
-  if (esp_now_init() != 0) {
-    return;
-  }
-  esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
-  esp_now_register_send_cb(OnDataSent);
-  esp_now_register_recv_cb(OnDataRecv);
-  
-  /* Registers the receiver of the message */
-  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_COMBO, WIFI_CHANNEL, NULL, 0);
-
-}
-
-/* Reads new robot_message from serial */
-void receiveUSBdata(){
-  int initCounter = 0;
-    
-  while(Serial.available()){
-    /* Reads a character from the message */
-    char character = Serial.read();
-
-    /* Increments the counter if it's 'T' */
-    if(character == 'T') initCounter++;
-
-    /* If the first characters are 'T', then in fact it is the beginning of the message */
-    if(initCounter >= 3){
-      SerialConstants receive;
-
-      /* Reads the message until the finishing character then decode it */
-      Serial.readBytes((char*)(&receive), (size_t)sizeof(SerialConstants));
-
-      /* Does the checksum */
-      int16_t checksum = 0;
-      checksum = receive.data.kp + receive.data.ki + receive.data.kp;
-      
-
-      /* Verifies o checksum */
-      if(checksum == receive.checksum){
-        /* Copies to the global velocity buffer */
-        send_commands = receive.data;
-        // robot_message = receive.data;
-      }
-
-      /* Reset the counter */
-      initCounter = 0;
-    }
-  }
-}
-
-#else
-
-/* Sends the message via Wi-Fi */
-void sendWifi(){
-
-  result = true;
-
-  for(uint8_t i=0 ; i<3 ; i++){
-    snd_message msg = {.control = (uint8_t)robot_message.control, .id = i, .v = robot_message.v[i], .w = robot_message.w[i]};
-    
-    /* Sends the message using ESP-NOW */
-    esp_now_send(broadcastAddress, (uint8_t *) &msg, sizeof(snd_message));
-    delay(3);
-  }
-  
-  if(result){
-    lastOK = millis();
-  }
-}
-
 /* Setup the Wi-Fi  */
 void wifiSetup(){
   /* Puts the device in Wi-Fi Station mode */
@@ -228,6 +98,23 @@ void wifiSetup(){
   
   /* Registers the receiver of the message */
   esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, WIFI_CHANNEL, NULL, 0);
+}
+
+/* Sends the message via Wi-Fi */
+void sendWifi(){
+  result = true;
+
+  for(uint8_t i=0 ; i<3 ; i++){
+    snd_message msg = {.control = (uint8_t)robot_message.control, .id = i, .v = robot_message.v[i], .w = robot_message.w[i]};
+    
+    /* Sends the message using ESP-NOW */
+    esp_now_send(broadcastAddress, (uint8_t *) &msg, sizeof(snd_message));
+    delay(3);
+  }
+  
+  if(result){
+    lastOK = millis();
+  }
 }
 
 /* Reads new robot_message from serial */
