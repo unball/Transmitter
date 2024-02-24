a73af4ed31052b4b9b866cca0d67c80bc1b739af
André <andredornelas23@gmail.com>
2024-02-24 15:31:48 -0300

    faz métodos receberem self

--- src/Twiddle.py
+++ src/Twiddle.py
@@ -11,13 +11,13 @@ class Twiddle():
     def closeSerial(self):
         if self.serial is not None: self.serial.close()      
 
-    def twiddle(k, dk, ksi=.3, target=None):
+    def twiddle(self, k, dk, ksi=.3, target=None):
         if not target:
-            target = run_pid_test(*k)
+            target = self.run_pid_test(*k)
 
         for i, _ in enumerate(k):
             k[i] += dk[i]
-            new_error = run_pid_test(*k)
+            new_error = self.run_pid_test(*k)
 
             if new_error < target:
                 target = new_error
@@ -25,7 +25,7 @@ class Twiddle():
 
             else:
                 k[i] -= 2*dk[i]
-                new_error = run_pid_test(*k)
+                new_error = self.run_pid_test(*k)
 
                 if new_error < target:
                     target = new_error
@@ -37,7 +37,7 @@ class Twiddle():
 
         return k, dk, ksi, target
 
-    def run_pid_test(kp, ki, kd):
+    def run_pid_test(self, p, ki, kd):
         print(f"<{0},{0},{0},{0},{3},{kp},{ki},{kd},{9},{0},{0},{0}>")
         esp32.write(f"<{0},{0},{0},{0},{3},{100*kp},{100*ki},{100*kd},{9},{0},{0},{0}>".encode())
         sleep(8)
