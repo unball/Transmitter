94dd5c5a4a22aa9b3e4ad28e70e6c0d5161c6a23
André <andredornelas23@gmail.com>
2024-02-24 15:30:42 -0300

    coloca métodos twiddle e run_pid_test dentro da classe

--- src/Twiddle.py
+++ src/Twiddle.py
@@ -9,24 +9,14 @@ class Twiddle():
         self.serial = None
 
     def closeSerial(self):
-        if self.serial is not None: self.serial.close()
+        if self.serial is not None: self.serial.close()      
 
-    
+    def twiddle(k, dk, ksi=.3, target=None):
+        if not target:
+            target = run_pid_test(*k)
 
-def twiddle(k, dk, ksi=.3, target=None):
-    if not target:
-        target = run_pid_test(*k)
-
-    for i, _ in enumerate(k):
-        k[i] += dk[i]
-        new_error = run_pid_test(*k)
-
-        if new_error < target:
-            target = new_error
-            dk[i] *= 1 + ksi
-
-        else:
-            k[i] -= 2*dk[i]
+        for i, _ in enumerate(k):
+            k[i] += dk[i]
             new_error = run_pid_test(*k)
 
             if new_error < target:
@@ -34,19 +24,27 @@ def twiddle(k, dk, ksi=.3, target=None):
                 dk[i] *= 1 + ksi
 
             else:
-                k[i] += dk[i]
-                dk[i] *= 1 - ksi
+                k[i] -= 2*dk[i]
+                new_error = run_pid_test(*k)
 
-    return k, dk, ksi, target
+                if new_error < target:
+                    target = new_error
+                    dk[i] *= 1 + ksi
 
-def run_pid_test(kp, ki, kd):
-    print(f"<{0},{0},{0},{0},{3},{kp},{ki},{kd},{9},{0},{0},{0}>")
-    esp32.write(f"<{0},{0},{0},{0},{3},{100*kp},{100*ki},{100*kd},{9},{0},{0},{0}>".encode())
-    sleep(8)
-    error = esp32.readline()
-    error = error.decode()
-    print(error)
-    return abs(float(error))
+                else:
+                    k[i] += dk[i]
+                    dk[i] *= 1 - ksi
+
+        return k, dk, ksi, target
+
+    def run_pid_test(kp, ki, kd):
+        print(f"<{0},{0},{0},{0},{3},{kp},{ki},{kd},{9},{0},{0},{0}>")
+        esp32.write(f"<{0},{0},{0},{0},{3},{100*kp},{100*ki},{100*kd},{9},{0},{0},{0}>".encode())
+        sleep(8)
+        error = esp32.readline()
+        error = error.decode()
+        print(error)
+        return abs(float(error))
 
 params = [[-1.858962031647976, -0.16864975434220458, 0.16686768215869935], [0.538265, 0.049981750000000005, 0.049981750000000005]]
 
