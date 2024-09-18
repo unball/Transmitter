06fde4df8dc0a1c1aaf2217487b2cf5f1985a730
Frederico <fredericofabricio5@gmail.com>
2024-09-18 18:56:17 -0300

    refac: tirado o PID_TUNNER e ajustes no parser e wifiSetup()

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include <esp_now.h>
 #include <esp_wifi.h>
 
-#define PID_TUNNER false
 
 /* Definitions */
 /*
@@ -29,9 +28,9 @@
 // a - {0xCC,0x8D,0xA2,0x8C,0x31,0xB6}
 // b - {0xCC,0x8D,0xA2,0x8B,0xCF,0xC8}
 
-uint8_t broadcastAddress[3][7] = {{0xCC,0x8D,0xA2,0x8B,0xD1,0x36},
-                                  {0xCC,0x8D,0xA2,0x8B,0xD1,0x50},
-                                  {0xCC,0x8D,0xA7,0x8B,0x41,0x36}};
+uint8_t broadcastAddress[3][6] = {{0xCC,0x8D,0xF2,0x6B,0xD0,0xCC},
+                                  {0xCC,0x8D,0xA2,0x8B,0xD1,0x36},
+                                  {0xCC,0x8D,0xF7,0x0B,0x81,0x36}};
 
 
 /* Estrutura para a mensagem a ser transmitida para o robô via wi-fi */
@@ -39,37 +38,6 @@ struct RobotMessage{
   int16_t v[3];
   int16_t w[3];
 };
-
-#if PID_TUNNER
-struct snd_message{
-  uint8_t id;
-  int16_t kp;
-  int16_t ki;
-  int16_t kd;
-};
-
-snd_message send_commands;
-
-struct rcv_message{
-  float value;
-};
-
-rcv_message rcv_commands;
-
-/* Struct for the message to be received from USB */
-struct SerialConstants {
-  snd_message data;
-  double checksum;
-};
-
-#else
-struct snd_message {
-  int8_t id;
-  int16_t v;
-  int16_t w;
-  int32_t checksum;
-};
-#endif
 
 /* Estrutura para a mensagem a ser recebida do USB */
 struct SerialMessage {
@@ -90,7 +58,7 @@ void receiveUSBdata();
 /* Contagem de erros de transmissão via USB detectados */
 uint32_t erros = 0;
 uint32_t lastOK = 0;
-bool result;
+bool result=false;
 
 //Callback when data is sent
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
@@ -99,15 +67,6 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   else
     result = false;
 }
-
-#if PID_TUNNER
-// Callback function, execute when message is sent via Wi-Fi
-void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len){
-  memcpy(&rcv_commands, incomingData, sizeof(rcv_commands));
-  Serial.println(rcv_commands.value);
-  delay(2);
-}
-#endif
 
 /* Loop de setup */
 void setup() {
@@ -138,110 +97,27 @@ void loop(){
 
 }
 
-#if PID_TUNNER
 /* Sends the message via Wi-Fi */
 void sendWifi(){
-
-  result = true;
-
   for(uint8_t i=0 ; i<3 ; i++){
-    snd_message control_constants = {.id = i, .kp = 1.0, .ki = 1.0, .kd = 1.0};
-    
-    /* Sends the message using ESP-NOW */
-    esp_now_send(broadcastAddress[i], (uint8_t *) &control_constants, sizeof(snd_message));
-    delay(3);
-  }
-  
-  if(result){
-    lastOK = millis();
-  }
-}
 
-/* Setup the Wi-Fi  */
-void wifiSetup(){
-  /* Puts the device in Wi-Fi Station mode */
-  WiFi.mode(WIFI_STA);
-
-  WiFi.setOutputPower(MAX_POWER);
-
-  /* Initialize the ESP-NOW */
-  if (esp_now_init() != 0) {
-    return;
-  }
-  esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
-  esp_now_register_send_cb(OnDataSent);
-  esp_now_register_recv_cb(OnDataRecv);
-  
-  /* Registers the receiver of the message */
-  for(int i =0; i<3; i++){
-    esp_now_add_peer(broadcastAddress[i], ESP_NOW_ROLE_COMBO, WIFI_CHANNEL, NULL, 0);
-  }
-
-}
-
-/* Reads new robot_message from serial */
-void receiveUSBdata(){
-  int initCounter = 0;
-    
-  while(Serial.available()){
-    /* Reads a character from the message */
-    char character = Serial.read();
-
-    /* Increments the counter if it's 'T' */
-    if(character == 'T') initCounter++;
-
-    /* If the first characters are 'T', then in fact it is the beginning of the message */
-    if(initCounter >= 3){
-      SerialConstants receive;
-
-      /* Reads the message until the finishing character then decode it */
-      Serial.readBytes((char*)(&receive), (size_t)sizeof(SerialConstants));
-
-      /* Does the checksum */
-      int16_t checksum = 0;
-      checksum = receive.data.kp + receive.data.ki + receive.data.kp;
-      
-
-      /* Verifies o checksum */
-      if(checksum == receive.checksum){
-        /* Copies to the global velocity buffer */
-        send_commands = receive.data;
-        // robot_message = receive.data;
-      }
-
-      /* Reset the counter */
-      initCounter = 0;
-    }
-  }
-}
-
-#else
-
-/* Sends the message via Wi-Fi */
-void sendWifi(){
-
-  result = true;
-
-  for(uint8_t i=0 ; i<3 ; i++){
-    
+    result = true;  // ??
     int32_t checksum = robot_message.v[i] + robot_message.w[i];
     int16_t limitedChecksum = checksum >= 0 ? (int16_t)(abs(checksum % 32767)) : -(int16_t)(abs(checksum % 32767));
-    
+
     // padrão da mensagem: "<id,v,w,checksum>"
     std::stringstream parser;
-    parser << "<" << i << "," << robot_message.v[i] << "," << robot_message.w[i] << "," << limitedChecksum << ">"<<"\0"; 
+    parser << '<' << (short int)i << ',' << robot_message.v[i] << ',' << robot_message.w[i] << ',' << limitedChecksum << '>' << '\0'; 
     //printf("%s\n",(parser.str()).c_str());
 
-    /* Sends the message using ESP-NOW */
-    esp_err_t result = esp_now_send(broadcastAddress[i], (uint8_t *) (parser.str()).c_str(), (parser.str()).size());
-    if (result == ESP_OK) {
+    esp_err_t sendResult = esp_now_send(broadcastAddress[i], (uint8_t *) (parser.str()).c_str(), (parser.str()).size());
+    if (sendResult == ESP_OK) {
     lastOK = millis();
     }
+
     delay(3);
   }
-
 }
-
 
 /* Setup the Wi-Fi  */
 void wifiSetup(){ 
@@ -254,9 +130,9 @@ void wifiSetup(){
         return;
     }
 
-  for(uint8_t i=0 ; i<3 ; i++){
-
   esp_now_register_send_cb(OnDataSent);
+
+  for(uint8_t i=0 ; i<3 ; i++){
 
   esp_now_peer_info_t peerInfo;
 
@@ -268,10 +144,9 @@ void wifiSetup(){
       return;
     }
   }
-  
-  
+
   esp_err_t error = esp_wifi_set_max_tx_power(20);
-  
+
   if (error == ESP_OK){
     return;
   }
@@ -332,4 +207,3 @@ void receiveUSBdata(){
   }
 }
 
-#endif
