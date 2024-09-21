35b958f69bef1d155a4bb3e6cf623d3be0358339
Frederico <fredericofabricio5@gmail.com>
2024-09-21 17:55:36 -0300

    improv: tirado alguns comentários e mudança na chave do parser

--- src/main.cpp
+++ src/main.cpp
@@ -5,29 +5,6 @@
 #include <esp_now.h>
 #include <esp_wifi.h>
 
-
-/* Definitions */
-/*
-#define MAX_POWWIFI_POWER_19_5dBmER 10.0  //TODO: Test values
-#define WIFI_CHANNEL 12  //TODO: Test values */
-
-/* Broadcast address, sends to everyone */
-// LISTA DE MAC DOS WEMOS8266
-// a - {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF}
-// b - {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF}
-// c - {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF}
-// 3 - {0x48,0x55,0x19,0xF6,0x44,0x44}
-// 4 - {0x5C,0xCF,0x7F,0xDE,0x1D,0x36}
-// 5 - {0x2C,0x3A,0xE8,0x00,0xDE,0xA0}
-// 6 - {0xC8,0xC9,0xA3,0x3A,0xDE,0x9E}
-// 7 - {0xB4,0xE6,0x2D,0x04,0x06,0x4F}
-// 8 - {0x98,0xCD,0xAC,0x39,0x70,0xBC}
-
-/* Broadcast address, sends to everyone */
-// LISTA DE MAC DOS WEMOS32
-// a - {0xCC,0x8D,0xA2,0x8C,0x31,0xB6}
-// b - {0xCC,0x8D,0xA2,0x8B,0xCF,0xC8}
-
 uint8_t deviceAddress[3][6] = { {0xCC,0x8D,0xF2,0x6B,0xD0,0xCC},
                                 {0xCC,0x8D,0xA2,0x8B,0xD1,0x36},
                                 {0xCC,0x8D,0xF7,0x0B,0x81,0x36} };
@@ -104,7 +81,7 @@ void sendWifi(){
     int16_t limitedChecksum = checksum >= 0 ? (int16_t)(abs(checksum % 32767)) : -(int16_t)(abs(checksum % 32767));
     // padrão da mensagem: "<id,v,w,checksum>"
     std::stringstream parser;
-    parser << '<' << (short int)i << ',' << robot_message.v[i] << ',' << robot_message.w[i] << ',' << limitedChecksum << '>' << '\0'; 
+    parser << '[' << (short int)i << ',' << robot_message.v[i] << ',' << robot_message.w[i] << ',' << limitedChecksum << ']' << '\0'; 
     //printf("%s\n",(parser.str()).c_str());
 
     esp_err_t sendResult = esp_now_send(deviceAddress[i], (uint8_t *) (parser.str()).c_str(), (parser.str()).size());
@@ -121,7 +98,7 @@ void sendWifi(){
       if (ackFlag) {
         // ACK recebido
         Serial.print("Mensagem recebida e ACK recebido do robô ");
-        Serial.print(i);
+        Serial.println(i);
         lastOK=millis();
       } else{
         // ACK não recebido no tempo estabelecido
