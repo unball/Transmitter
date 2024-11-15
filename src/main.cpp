0297df3fed35ad6e4e8b381ce815b4c84c5ce541
MariaClaudia1328 <mariaclaudia8.0@hotmail.com>
2024-11-15 15:22:42 -0300

    ajjusta pra enviar struct

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,43 @@
-#include <Arduino.h>
-#include <cstdint>
-#include <sstream>
 #include <SPI.h>
 #include <WiFi.h>
 #include <esp_now.h>
-#include <esp_wifi.h>
-#include <iostream>
-#include <string>
+#include <esp_wifi.h> 
 
-#define DEBUG 0
+#define PID_TUNNER false
 
-uint8_t deviceAddress[3][6] = { {0xCC,0x8D,0xA2,0x8D,0x0D,0x7C},
-                                {0xCC,0x8D,0xA2,0x8B,0xD1,0x36},
-                                {0x80,0x65,0x99,0xFC,0x40,0xCC} };
+/* Definitions */
+#define MAX_POWER 10.0  //TODO: Test values
+#define WIFI_CHANNEL 12  //TODO: Test values
 
-/* Estrutura para a mensagem a ser transmitida para o robô via Wi-Fi */
-struct RobotMessage {
+/* Broadcast address, sends to everyone */
+// LISTA DE MAC DOS WEMOS8266
+// a - {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF}
+// b - {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF}
+// c - {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF}
+// 3 - {0x48,0x55,0x19,0xF6,0x44,0x44}
+// 4 - {0x5C,0xCF,0x7F,0xDE,0x1D,0x36}
+// 5 - {0x2C,0x3A,0xE8,0x00,0xDE,0xA0}
+// 6 - {0xC8,0xC9,0xA3,0x3A,0xDE,0x9E}
+// 7 - {0xB4,0xE6,0x2D,0x04,0x06,0x4F}
+// 8 - {0x98,0xCD,0xAC,0x39,0x70,0xBC}
+
+uint8_t broadcastAddress[3][7] = {{0xA0,0xDD,0x6C,0x04,0x7E,0x0C},
+                                  {0xC8, 0xC9, 0xA3, 0x3A, 0xDE, 0x9E},
+                                  {0x2C, 0x3A, 0xE8, 0x00, 0xDE, 0xA0}};
+
+/* Estrutura para a mensagem a ser transmitida para o robô via wi-fi */
+struct RobotMessage{
   int16_t vx;
   int16_t vy;
   int16_t w;
+};
+
+struct snd_message{
+  int16_t id;
+  int16_t vx;
+  int16_t vy;
+  int16_t w;
+  int16_t checksum;
 };
 
 /* Estrutura para a mensagem a ser recebida do USB */
@@ -27,63 +46,58 @@ struct SerialMessage {
   int16_t checksum;
 };
 
-/* Mensagem a ser transmitida */
-RobotMessage robot_message;
-
-esp_now_peer_info_t peerInfo;
-
 /* Declaração das funções */
 void wifiSetup();
 void sendWifi();
 void receiveUSBdata();
-void detectKeyPressAndSend();
+
+/* Mensagem a ser transmitida */
+RobotMessage robot_message;
 
 /* Contagem de erros de transmissão via USB detectados */
 uint32_t erros = 0;
 uint32_t lastOK = 0;
-bool ackFlag = false;
+bool result;
 
-// Variáveis para velocidades de movimento
-float x_dot = 0.0;   // Velocidade na direção x
-float y_dot = 0.0;   // Velocidade na direção y
-float omega_dot = 0.0;  // Velocidade angular
-
-// Callback when data is sent
-#if DEBUG
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  ackFlag = (status == ESP_NOW_SEND_SUCCESS);
+//Callback when data is sent
+void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus)
+{
+  if (sendStatus == 0)
+    result = true;
+  else
+    result = false;
 }
-#endif
 
 /* Loop de setup */
-void setup() {
+void setup(void) {
   Serial.begin(115200);
+  while(!Serial);
   wifiSetup();
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 /* Loop que é executado continuamente */
-void loop() {
+void loop(){
     // Recebe robot_message via USB
-    receiveUSBdata();
+    // receiveUSBdata();
+    detectKeyPressAndSend();
 
-    // Envia via Wi-Fi
     static int32_t t = micros();
-    if (micros() - t >= 100) {
+    if(micros()-t >= 500){
         t = micros();
         sendWifi();
     }
 
-    // Acende o LED se recebeu mensagem do USB em menos de 40ms
-    if (millis() - lastOK < (DEBUG ? 40 : 5)) {
-        digitalWrite(LED_BUILTIN, HIGH);
-    } else {
-        digitalWrite(LED_BUILTIN, LOW);
+    // Acende o LED se recebeu mensagem do USB em menos de 5ms
+    if(millis()-lastOK < 5){
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+    else{
+      digitalWrite(LED_BUILTIN, LOW);
     }
 
-    // Detecta a tecla pressionada e envia o comando correspondente
-    detectKeyPressAndSend();
 }
+
 
 void detectKeyPressAndSend() {
     if (Serial.available()) {
@@ -93,125 +107,127 @@ void detectKeyPressAndSend() {
 
         // Interpreta o comando
         if (input == "W" || "w") {  // Frente
-            robot_message.vx = 0;
-            robot_message.vy = 1;
-            robot_message.w = 0;
-            Serial.println("Comando: Frente");
-        } else if (input == "A" || "a") {  // Esquerda
-            robot_message.vx = -1;
-            robot_message.vy = 0;
-            robot_message.w = -0.5;
-            Serial.println("Comando: Esquerda");
-        } else if (input == "S" || "s") {  // Ré
-            robot_message.vx = 0;
-            robot_message.vy = -1;
-            robot_message.w = 0;
-            Serial.println("Comando: Ré");
-        } else if (input == "D" || "d") {  // Direita
+            
             robot_message.vx = 1;
             robot_message.vy = 0;
             robot_message.w = 0;
+            Serial.println("Comando: Frente");
+        } else if (input == "A" || "a") {  // Esquerda
+            robot_message.vx = 0;
+            robot_message.vy = 1;
+            robot_message.w = 0;
+            Serial.println("Comando: Esquerda");
+        } else if (input == "S" || "s") {  // Ré
+            robot_message.vx = -1;
+            robot_message.vy = 0;
+            robot_message.w = 0;
+            Serial.println("Comando: Ré");
+        } else if (input == "D" || "d") {  // Direita
+            robot_message.vx = 0;
+            robot_message.vy = -1;
+            robot_message.w = 0;
             Serial.println("Comando: Direita");
         } else {
-            Serial.println("Comando desconhecido");
+            Serial.println("Para");
+            robot_message.vx = 0;
+            robot_message.vy = 0;
+            robot_message.w = 0;
         }
 
         // Envia o comando atualizado para o robô
-        sendWifi();
+        // sendWifi();
     }
 }
 
 /* Sends the message via Wi-Fi */
-void sendWifi() {
-    for (uint8_t i = 0; i < 3; i++) {
-        int32_t checksum = robot_message.vx + robot_message.vy + robot_message.w;
-        int16_t limitedChecksum = checksum >= 0 ? (int16_t)(abs(checksum % 32767)) : -(int16_t)(abs(checksum % 32767));
+void sendWifi(){
 
-        std::stringstream parser;
-        parser << '[' << (short int)i << ',' << robot_message.vx << ',' << robot_message.vy << ',' << robot_message.w << limitedChecksum << ']' << '\0';
+  result = true;
 
-        esp_err_t sendResult = esp_now_send(deviceAddress[i], (uint8_t *)(parser.str()).c_str(), (parser.str()).size());
+  for(uint8_t i=0 ; i<1 ; i++){
+    
+    int32_t checksum = robot_message.vx +  robot_message.vy + robot_message.w;
+    int16_t limitedChecksum = checksum >= 0 ? (int16_t)(abs(checksum % 32767)) : -(int16_t)(abs(checksum % 32767));
 
-        if (sendResult == ESP_OK) {
-            #if DEBUG
-            constexpr unsigned long TIMEOUT_MS = 30;
-            unsigned long startTime = millis();
-
-            while (!ackFlag && (millis() - startTime) < TIMEOUT_MS) {
-                vTaskDelay(pdMS_TO_TICKS(1));
-            }
-
-            if (ackFlag) {
-                Serial.print("Mensagem recebida e ACK recebido do robô ");
-                Serial.println(i);
-                lastOK = millis();
-            } else {
-                Serial.print("Falha ao enviar ou não foi recebido o ACK para o robô ");
-                Serial.println(i);
-            }
-            ackFlag = false;
-            #else
-            lastOK = millis();
-            #endif
-        }
-        vTaskDelay(pdMS_TO_TICKS(3));
-    }
+    snd_message msg = {.id = i, .vx = robot_message.vx, .vy = robot_message.vy, .w = robot_message.w, .checksum = limitedChecksum};
+    
+    /* Sends the message using ESP-NOW */
+    esp_now_send(broadcastAddress[i], (uint8_t *) &msg, sizeof(snd_message));
+    delay(3);
+  }
+  
+  if(result){
+    lastOK = millis();
+  }
 }
 
-/* Setup the Wi-Fi */
-void wifiSetup() { 
+
+/* Setup the Wi-Fi  */
+void wifiSetup(){ 
+  /* Puts the device in Wi-Fi Station mode */
   WiFi.mode(WIFI_STA);
 
+  /* Initialize the ESP-NOW */
   if (esp_now_init() != ESP_OK) {
         Serial.println("Erro ao inicializar o ESP-NOW");
         return;
-  }
-
-  #if DEBUG
-  esp_now_register_send_cb(OnDataSent);
-  #endif
-
-  for (uint8_t i = 0; i < 3; i++) {
-    memcpy(peerInfo.peer_addr, deviceAddress[i], 6);
-    peerInfo.encrypt = false;
-
-    if (esp_now_add_peer(&peerInfo) != ESP_OK) {  
-        Serial.println("Erro ao adicionar o peer");
-        return;
     }
-  }
 
-  esp_wifi_set_max_tx_power(20);
-  esp_wifi_set_channel(14, WIFI_SECOND_CHAN_NONE);
+  esp_now_peer_info_t peerInfo;
+
+  memcpy(peerInfo.peer_addr, broadcastAddress[0], 6);
+  peerInfo.encrypt = false;
+
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {  
+      Serial.println("Erro ao adicionar o peer");
+      return;
+    }
+
+  esp_err_t error = esp_wifi_set_max_tx_power(20);
 }
 
 /* Reads new robot_message from serial */
-void receiveUSBdata() {
-    int initCounter = 0;
+void receiveUSBdata(){
+  int initCounter = 0;
+    
+  while(Serial.available()){
+    /* Lê um caracter da mensagem */
+    char character = Serial.read();
 
-    while (Serial.available()) {
-        char character = Serial.read();
+    /* Incrementa o contador se for 'B' */
+    if(character == 'B') initCounter++;
 
-        if (character == 'B') initCounter++;
+    /* Se os três primeiros caracteres são 'B' então de fato é o início da mensagem */
+    if(initCounter >= 3){
+      SerialMessage receive;
+      
+      /* Lê a mensagem até o caracter de terminação e a decodifica */
+      Serial.readBytes((char*)(&receive), (size_t)sizeof(SerialMessage));
 
-        if (initCounter >= 3) {
-            SerialMessage receive;
-            Serial.readBytes((char *)(&receive), (size_t)sizeof(SerialMessage));
+      /* Faz o checksum */
+      int16_t checksum = 0;
+      checksum += receive.data.vx + receive.data.vy + receive.data.w;
 
-            int32_t checksum = 0;
-            checksum += receive.data.vx + receive.data.vy + receive.data.w;
+      /* Verifica o checksum */
+      if(checksum == receive.checksum){
+        /* Copia para o buffer global de robot_message */
+        robot_message = receive.data;
 
-            int16_t limitedChecksum = checksum >= 0 ? (int16_t)(abs(checksum % 32767)) : -(int16_t)(abs(checksum % 32767));
-
-            if (limitedChecksum == receive.checksum) {
-                robot_message = receive.data;
-                Serial.printf("%d\t%d\t%d\n", limitedChecksum, robot_message.vx, robot_message.vy, robot_message.w);
-            } else {
-                for (uint16_t i = 0; i < sizeof(SerialMessage); i++) {
-                    Serial.printf("%p ", ((char *)&receive)[i]);
-                }
-            }
-            initCounter = 0;
+        /* Reporta que deu certo */
+        Serial.printf("%d\t%d\t%d\n", checksum, robot_message.vx, robot_message.vy, robot_message.w);
+        
+      }
+      else {
+        /* Devolve o checksum calculado se deu errado */
+        for(uint16_t i=0 ; i<sizeof(SerialMessage) ; i++){
+          Serial.printf("%p ", ((char*)&receive)[i]);
         }
+        Serial.println("");
+        //Serial.printf("%p\t%p\t%p\n", checksum, robot_message.v[0], robot_message.w[0]);
+      }
+
+      /* Zera o contador */
+      initCounter = 0;
     }
+  }
 }
